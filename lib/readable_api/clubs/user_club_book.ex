347f4c79e38de10e11f7e9e87202564f04be5690
raymondboswel <raymondboswel@gmail.com>
2021-05-01 20:03:19 +0200

    Updated schema for user club books

--- lib/readable_api/clubs/user_club_book.ex
+++ lib/readable_api/clubs/user_club_book.ex
@@ -1,11 +1,14 @@
 defmodule ReadableApi.Clubs.UserClubBook do
   use Ecto.Schema
   import Ecto.Changeset
+  alias ReadableApi.Clubs.Club
+  alias ReadableApi.Library.Book
+  alias ReadableApi.Accounts.User
 
   schema "user_club_books" do
-    field :user_id, :id
-    field :book_id, :id
-    field :club_id, :id
+    belongs_to(:user, User)
+    belongs_to(:book, Book)
+    belongs_to(:club, Club)
 
     timestamps()
   end
@@ -13,7 +16,7 @@ defmodule ReadableApi.Clubs.UserClubBook do
   @doc false
   def changeset(user_club_book, attrs) do
     user_club_book
-    |> cast(attrs, [])
-    |> validate_required([])
+    |> cast(attrs, [:user_id, :book_id, :club_id])
+    |> validate_required([:user_id, :book_id, :club_id])
   end
 end
